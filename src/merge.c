d722a02e26aa91185dc1185bfca42fb7afff0b7e
sdi2100100 <sdi2100100@di.uoa.gr>
2023-12-17 13:27:08 +0200

    improved merge

--- src/merge.c
+++ src/merge.c
@@ -3,64 +3,70 @@
 #include <string.h>
 #include <stdbool.h>
 
-void merge(int input_FileDesc, int chunkSize, int bWay, int output_FileDesc ){
-    int totalBlocks;
-    BF_GetBlockCounter(input_FileDesc, &totalBlocks);
-    int numChunks = totalBlocks / (chunkSize * bWay);
-    if (totalBlocks % (chunkSize * bWay) != 0) {
-        numChunks++;
+
+
+
+
+void merge(int input_FileDesc, int chunkSize, int bWay, int output_FileDesc) {
+    // Create a CHUNK iterator to traverse through input chunks
+    CHUNK_Iterator chunkIterator = CHUNK_CreateIterator(input_FileDesc, chunkSize);
+    CHUNK* ch=malloc(sizeof(CHUNK)* bWay);
+
+    // Allocate memory for storing records and cursors for each chunk
+    int* cursors = malloc(sizeof(int) * bWay);
+
+    // Initialize cursors for each chunk
+    for (int i = 0; i < bWay; ++i) {
+        cursors[i] = -1;
     }
 
-    // Perform merging
-    for (int i = 0; i < numChunks; ++i) {
-        int numBlocksToMerge = chunkSize * bWay;
-        if ((i + 1) * chunkSize * bWay > totalBlocks) {
-            numBlocksToMerge = totalBlocks - i * chunkSize * bWay;
-        }
-
-        // Merge b chunks into a new chunk
-        CHUNK newChunk;
-        newChunk.file_desc = output_FileDesc;
-        newChunk.from_BlockId = (i * chunkSize * bWay) + 1;
-        newChunk.to_BlockId = newChunk.from_BlockId + numBlocksToMerge - 1;
-        newChunk.blocksInChunk = numBlocksToMerge;
-        
-        // Create an iterator for merging b chunks
-        CHUNK_Iterator iterator = CHUNK_CreateIterator(input_FileDesc, chunkSize);
-        CHUNK chunk;
-        int chunksMerged = 0;
-
-        // Merge b chunks into a new chunk
-        while (chunksMerged < bWay && CHUNK_GetNext(&iterator, &chunk)) {
-            for (int blockId = chunk.from_BlockId; blockId <= chunk.to_BlockId; ++blockId) {
-                BF_Block* block;
-                BF_Block_Init(&block);
-                BF_GetBlock(input_FileDesc, blockId, block);
-
-                // Write the block's data to the new chunk
-                BF_Block* newBlock;
-                BF_Block_Init(&newBlock);
-                BF_AllocateBlock(output_FileDesc, newBlock);
-                char* data = BF_Block_GetData(block);
-                char* newData = BF_Block_GetData(newBlock);
-                memcpy(newData, data, BF_BLOCK_SIZE);
-
-                BF_Block_SetDirty(newBlock);
-                BF_UnpinBlock(newBlock);
-                BF_UnpinBlock(block);
-                BF_Block_Destroy(&block);
-                BF_Block_Destroy(&newBlock);
+    while (true) {
+        bool anyRecords = false;
+        // Load new chunks or check records in the loaded chunks
+        for (int i = 0; i < bWay; ++i) {
+            if (cursors[i] == -1) {  // Load new chunk
+                if (CHUNK_GetNext(&chunkIterator, &ch[i])) {
+                    if(ch[i].recordsInChunk!=0){
+                        cursors[i] = 0;
+                        CHUNK_RecordIterator recordIterator = CHUNK_CreateRecordIterator(&ch[i]);
+                        anyRecords = true;
+                    }
+                }
+            } else {  // Check records in the loaded chunks
+                anyRecords = true;
             }
-            chunksMerged++;
         }
 
-        // Clean up
-        CHUNK_Print(newChunk);
-        BF_Block* lastBlock;
-        BF_Block_Init(&lastBlock);
-        BF_GetBlock(output_FileDesc, newChunk.to_BlockId, lastBlock);
-        BF_UnpinBlock(lastBlock);
-        BF_Block_Destroy(&lastBlock);
+        if (!anyRecords) {
+            break;  // No more records across all chunks
+        }
+
+        int minposition = -1;
+        Record minRecord;
+
+        // Find the minimum record among the available records in the chunks
+        Record current;
+        for (int i = 0; i < bWay; ++i) {
+            if(cursors[i]!=-1){
+                CHUNK_GetIthRecordInChunk(&ch[i],  cursors[i], &current);
+                if (minposition == -1 || shouldSwap(&current, &minRecord)) {
+                    minRecord = current;
+                    minposition = i;
+                }
+            }
+        }
+        
+        // Write the minRecord to the output file
+        HP_InsertEntry(output_FileDesc, minRecord);
+
+        // Move to the next record in the chunk
+        cursors[minposition]++;
+        if (cursors[minposition] >= ch[minposition].recordsInChunk) {
+            cursors[minposition] = -1;  // Chunk exhausted
+        }
     }
-    
+
+    // Free allocated memory
+    free(ch);
+    free(cursors);
 }
